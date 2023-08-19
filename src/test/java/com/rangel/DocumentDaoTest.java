49a00ed917cc0d9c56108935c4639bd86909b0a5
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-19 17:04:47 -0300

    DocumentDaoTest implemented

--- src/test/java/com/rangel/DocumentDaoTest.java
+++ src/test/java/com/rangel/DocumentDaoTest.java
@@ -37,11 +37,72 @@ public class DocumentDaoTest {
         assertEquals("787", result.get(0).getCbfNumber());
     }
 
+    @Test
+    @DisplayName("2 - Must perform Document Listing.")
+    public void mustList() {
+
+        // SCENERY BUILDING AND ACTION CALL
+        for (int i = 0; i <= 5; i++) {
+            this.documentDao.save(this.createObject("544", "544554454", "787"));
+        }
+
+        // VERIFICATION OF EXPECTED RESULT
+        List<Document> result = this.documentDao.list();
+
+        assertEquals(6, result.size());
+    }
+
+    @Test
+    @DisplayName("3 - Must Edit a Document.")
+    public void mustEdit() {
+        this.documentDao.save(this.createObject("544", "544554454", "787"));
+
+        Document result = this.documentDao.listar().get(0);
+
+        result.setCpf("11111");
+
+        this.documentDao.edit(result);
+
+        // VERIFICATION OF EXPECTED RESULT
+        List<Document> resultList = this.documentDao.listar();
+
+        assertEquals(1, resultList.size());
+        assertEquals("544", resultList.get(0).getWordCardNumber());
+        assertEquals("11111", resultList.get(0).getCpf());
+        assertEquals("787", resultList.get(0).geCbfNumber());
+
+    }
+
+    @Test
+    @DisplayName("4 - Must Delete a Document.")
+    public void mustDelete() {
+        this.documentDao.save(this.createObject("544", "544554454", "787"));
+
+        Document result = this.documentDao.listar().get(0);
+
+        this.documentDao.delete(result.getId());
+
+        // VERIFICATION OF EXPECTED RESULT
+        List<Document> resultList = this.documentDao.listar();
+
+        assertEquals(0, resultList.size());
+
+    }
+
     private void clearData() {
         List<Document> objects = this.documentDao.list();
 
         for (Document document : objects) {
             this.documentDao.delete(document.getId());
         }
+    }
+
+    private Document createObject(String workCardNumber, String cpf, String cbfNumber) {
+        Document document = new Document();
+        document.setWordCardNumber(workCardNumber);
+        document.setCpf(cpf);
+        document.setCbfNumber(cbfNumber);
+
+        return document;
     }
 }
