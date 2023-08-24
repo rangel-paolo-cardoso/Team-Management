0596d9b5a60463c4bf522557d04418c16a51d445
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-23 22:01:28 -0300

    Implemented TeamDao class

--- src/main/java/com/rangel/dao/TeamDao.java
+++ src/main/java/com/rangel/dao/TeamDao.java
@@ -2,28 +2,44 @@ package com.rangel.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import com.rangel.model.Team;
 
 public class TeamDao extends GenericDao<Team, Integer> {
 
+    EntityManager em = new ApplicationDB().getEntityManager();
+
     @Override
     public void save(Team obj) {
-        // 
+        em.getTransaction().begin();
+        em.persist(obj);
+        em.getTransaction().commit();
     }
 
     @Override
     public List<Team> list() {
-        // 
+        List<Team> teams;
+        TypedQuery<Team> query = em.createQuery("SELECR t FROM t", Team.class);
+        teams = query.getResultList();
+        return teams;
     }
 
     @Override
     public void edit(Team obj) {
-        // 
+        em.getTransaction().begin();
+        em.merge(obj);
+        em.getTransaction().commit();
     }
 
     @Override
     public void delete(Integer id) {
-        // 
+        Team team = em.find(Team.class, id);
+
+        em.getTransaction().begin();
+        em.remove(team);
+        em.getTransaction().commit();
     }
     
 }
