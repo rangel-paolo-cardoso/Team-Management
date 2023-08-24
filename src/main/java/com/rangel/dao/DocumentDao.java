82c0d1df8362ade202a8f68592b2255d828f0873
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-23 21:59:19 -0300

    Implemented PlayerDao class

--- src/main/java/com/rangel/dao/DocumentDao.java
+++ src/main/java/com/rangel/dao/DocumentDao.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import javax.print.Doc;
 
 import com.rangel.model.Document;
 
--- src/main/java/com/rangel/dao/PlayerDao.java
+++ src/main/java/com/rangel/dao/PlayerDao.java
@@ -2,28 +2,44 @@ package com.rangel.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import com.rangel.model.Player;
 
 public class PlayerDao extends GenericDao<Player, Integer> {
 
+    EntityManager em = new ApplicationDB().getEntityManager();
+
     @Override
     public void save(Player obj) {
-        // 
+        em.getTransaction().begin();
+        em.persist(obj);
+        em.getTransaction().commit();
     }
 
     @Override
     public List<Player> list() {
-        // 
+        List<Player> players;
+        TypedQuery<Player> query = em.createQuery("SELECT p FROM p", Player.class);
+        players = query.getResultList();
+        return players;
     }
 
     @Override
     public void edit(Player obj) {
-        // 
+        em.getTransaction().begin();
+        em.merge(obj);
+        em.getTransaction().commit();
     }
 
     @Override
     public void delete(Integer id) {
-        // 
+        Player player = em.find(Player.class, id);
+
+        em.getTransaction().begin();
+        em.remove(player);
+        em.getTransaction().commit();
     }
     
 }
