a5d6214af14ee04b6e5cb3e30f61fd3449525340
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-08-23 21:57:26 -0300

    Implemented DocumentDao class

--- src/main/java/com/rangel/dao/DocumentDao.java
+++ src/main/java/com/rangel/dao/DocumentDao.java
@@ -2,28 +2,45 @@ package com.rangel.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.print.Doc;
+
 import com.rangel.model.Document;
 
 public class DocumentDao extends GenericDao<Document, Integer> {
 
+    EntityManager em = new ApplicationDB().getEntityManager();
+
     @Override
     public void save(Document obj) {
-        // 
+        em.getTransaction().begin();
+        em.persist(obj);
+        em.getTransaction().commit();
     }
 
     @Override
     public List<Document> list() {
-        // 
+        List<Document> documents;
+        TypedQuery<Document> query = em.createQuery("SELECR d FROM d", Document.class);
+        documents = query.getResultList();
+        return documents;
     }
 
     @Override
     public void edit(Document obj) {
-        // 
+        em.getTransaction().begin();
+        em.merge(obj);
+        em.getTransaction().commit();
     }
 
     @Override
     public void delete(Integer id) {
-        // 
+        Document document = em.find(Document.class, id);
+
+        em.getTransaction().begin();
+        em.remove(document);
+        em.getTransaction().commit();
     }
     
 }
